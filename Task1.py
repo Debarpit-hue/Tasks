abfdc2d3a671a200af0dfb4990cb024bcffedf1a
Debarpit-hue <debarpitsamal888@gmail.com>
2025-07-04 16:03:10 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=input("write a number: ")
+b=input("write a number: ")
+a=int(a)
+b=int(b)
+c=a+b
+print("Addition: ",c)
+c=a-b
+print("Subtraction: ",c)
+c=a*b
+print("Multiplication: ",c)
+c=a/b
+print("Division: ",c)
